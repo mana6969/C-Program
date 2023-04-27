b95385dfbaf2c530fd83d3e66a9b44563882620d
Chitta Ranjan Ojha <129220280+mana6969@users.noreply.github.com>
2023-04-28 00:40:23 +0530

    digit sum of a number

--- digit_sum_of_num.cpp
+++ digit_sum_of_num.cpp
@@ -9,26 +9,23 @@ int main() {
     scanf("%d", &n);
     int i,sum,count=0,temp,flag;
     temp=n;
-    flag=n=temp;
+    flag=n;
     while(n>0)
     {
         n=n/10;
         count=count+1;
     }
-    printf("\n%d",count);
     int a[count];
     for(i=0;i<count;i++)
     {
         temp=flag%10;
         a[i]=temp;
         flag=flag/10;
-        printf("\n%d",flag);
     }
     sum=0;
     for(i=0;i<count;i++)
     {
         sum=sum+a[i];
-        printf("\n%d",sum);
     }
     printf("%d",sum);
     return 0;
